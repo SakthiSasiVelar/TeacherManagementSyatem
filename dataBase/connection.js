5719e25e403e2f79e6c9ed68cb672726524ee67b
unknown <sakthisasivelar@gmail.com>
2024-02-07 13:43:19 +0530

    commit

--- dataBase/connection.js
+++ dataBase/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectToDataBase = async function () {
   try {
     const uri =
-      "mongodb+srv://srinith:hmEq1RRQ2W9bCJ3f@cluster0.ggoqz7o.mongodb.net/?retryWrites=true&w=majority";
+      "mongodb+srv://teacherManagementportal:portal123@cluster0.lf7zi6d.mongodb.net/?retryWrites=true&w=majority";
     mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
